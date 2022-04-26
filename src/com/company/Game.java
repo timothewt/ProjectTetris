d5c2cf21253a015f799aa92b996003399e33342c
Timothe <timothe.wt@gmail.com>
2022-04-26 23:52:34 +0200

    idk

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -1,12 +1,15 @@
 package com.company;
 import model.GameModel;
 
+/**
+ * The main() method of this class is the one that is executed when the program starts
+ */
 public class Game {
 
     public static void main(String[] args) {
 
-        GameModel game = new GameModel();
-        game.start();
+        GameModel game = new GameModel(); // new instance of a game
+        game.start(); // starts the game
 
     }
 
